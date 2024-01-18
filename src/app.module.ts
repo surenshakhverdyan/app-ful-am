68a2a9a87d5366d6035121faf941cd5ec2a75cdd
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-01-18 18:27:45 +0400

    configure app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,30 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        transport: `smtps://${configService.get<string>('EMAIL_ADDRESS')}:${configService.get<string>('EMAIL_PASSWORD')}@${configService.get<string>('EMAIL_HOST')}`,
+      }),
+    }),
+    MulterModule.register({
+      dest: 'uploads',
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
